bfd34f40f72034e38e67c038b4ffe2d349b3279c
fukiko <kinako.fukiko.0822@gmail.com>
2022-06-09 21:21:06 +0900

    一覧ページ

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,15 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample_app/todo_detail.dart';
 
 // 一覧ページ用ウィジェット
-class TodoListPage extends StatelessWidget {
+class TodoListPage extends StatefulWidget {
     const TodoListPage({Key? key}) : super(key: key);
 
     @override
+    TodoListPageState createState() => TodoListPageState();
+}
+
+class TodoListPageState extends State {
+    final List<String> showType = ['未完了', '完了'];  // タブの名前を表示するための配列
+    int bottomBarIndex = 0;  // 選択されたタブを管理するための変数
+    List<Map> todoList = [
+        {
+            'title': 'Flutter',
+            'content': 'Flutter を勉強する',
+            'isCompleted': false,
+        },
+        {
+            'title': '買い物',
+            'content': '卵を買う',
+            'isCompleted': false,
+        },
+        {
+            'title': '課題',
+            'content': '月 3 レポート',
+            'isCompleted': true,
+        },
+    ];
+
+    // ボトムバーがタップされたら setState を実行
+    void onBottomBarTapped(int index) {
+        setState(() {
+            bottomBarIndex = index;
+        });
+    }
+
+    void toDetailPage() {
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+                return const TodoDetailPage();
+            }),
+        );
+    }
+
+    @override
     Widget build(BuildContext context) {
-        return const Scaffold(
-            body: Center(
-                child: Text('リスト一覧画面'),
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text('ToDo 一覧'),
             ),
+            body: Center(
+                child: ListView.builder(
+                    itemCount: todoList.length,
+                    itemBuilder: (context, index) {
+                        return Card(
+                            child: ListTile(
+                                title: Text(
+                                    '${index + 1}  ${todoList[index]['title']} ${
+                                        todoList[index]['isCompleted']
+                                        ? 'true' : 'false'
+                                    }'
+                                )
+                            ),
+                        );
+                    },
+                ),
+            ),
+            floatingActionButton: FloatingActionButton(
+                onPressed: toDetailPage,
+                child: const Icon(Icons.add),
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.unpublished),
+                        label: '未完了',
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.check_circle),
+                        label: '完了',
+                    ),
+                ],
+                onTap: onBottomBarTapped,
+                currentIndex: bottomBarIndex,
+                selectedItemColor: Colors.blue.shade900,
+            )
         );
     }
 }
