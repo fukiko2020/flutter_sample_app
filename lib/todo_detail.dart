01e95b84e4b0763c11cfae5b1ba0769fe342c4b1
fukiko <kinako.fukiko.0822@gmail.com>
2022-06-20 16:23:16 +0900

    improve todo_detail

--- lib/todo_detail.dart
+++ lib/todo_detail.dart
@@ -48,7 +48,7 @@ class TodoDetailPageState extends State<TodoDetailPage> {
         title: const Text('Todo 詳細'),
       ),
       body: Center(
-        child: Container(
+        child: SizedBox(
           width: 300,
           child: Column(
             mainAxisSize: MainAxisSize.min,
@@ -56,35 +56,29 @@ class TodoDetailPageState extends State<TodoDetailPage> {
               Container(
                 alignment: Alignment.centerLeft,
                 margin: const EdgeInsets.only(bottom: 4.0),
-                // width: 100.0,
                 child: const Text('タイトル'),
               ),
-              Container(
+              Card(
                 margin: const EdgeInsets.only(bottom: 32.0),
-                padding: const EdgeInsets.all(4.0),
-                width: 300,
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(width: 1.0),
-                  ),
-                ),
-                child: Text(widget.todoItem.title),
+                child: Container(
+                  margin: const EdgeInsets.all(12.0),
+                  width: 300,
+                  child: Text(widget.todoItem.title),
+                )
               ),
               Container(
                 alignment: Alignment.centerLeft,
                 margin: const EdgeInsets.only(bottom: 8.0),
                 child: const Text('内容'),
               ),
-              Container(
+              Card(
                 margin: const EdgeInsets.only(bottom: 32.0),
-                padding: const EdgeInsets.all(4.0),
-                width: 300,
-                height: 200,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1),
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                child: Text(widget.todoItem.content),
+                child: Container(
+                  margin: const EdgeInsets.all(8.0),
+                  width: 300,
+                  height: 200,
+                  child: Text(widget.todoItem.content),
+                )
               ),
               SizedBox(
                 width: 300,
